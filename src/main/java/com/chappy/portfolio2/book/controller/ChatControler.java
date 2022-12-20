3d517cafa041ce77cac217c3980e785b78354f7e
chappy <s.erk.ikt@gmail.com>
2022-12-20 10:50:52 +0900

    Create validation

--- src/main/java/com/chappy/portfolio2/book/controller/ChatControler.java
+++ src/main/java/com/chappy/portfolio2/book/controller/ChatControler.java
@@ -2,6 +2,8 @@ package com.chappy.portfolio2.book.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,12 @@ public class ChatControler {
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute Chat chat){
+    public String create(@Validated @ModelAttribute Chat chat, BindingResult result){
+
+        //バリデーションエラーがある場合はindex.htmlを表示
+        if (result.hasErrors()) {
+            return "chat/index";
+        }
         return "chat/create";
     }
 
--- src/main/java/com/chappy/portfolio2/book/model/Chat.java
+++ src/main/java/com/chappy/portfolio2/book/model/Chat.java
@@ -1,11 +1,16 @@
 package com.chappy.portfolio2.book.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Chat {
+    @NotBlank
+    @Size(max = 20)
     private String name;
 
 }
